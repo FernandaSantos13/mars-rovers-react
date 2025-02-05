3751124490c22d446f6fe64878f86a5f836442f9
FernandaSantos13 <fernandacsantos.bio@gmail.com>
2025-02-04 23:40:01 -0500

    Refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,55 @@
 import { useState } from 'react'
-import { Rover } from './Rover'
-
+import { createPlateau, readInitialPosition, recordMoves } from './Rover'
+import { Rover, Plateau } from './Rover'
 
 const App = () => {
-  //const [count, setCount] = useState(0)
-  
-  const handleSimulation = () => {
-    return (
-      console.log("Simulating...")
-    );
+  const [instructions, setInstructions] = useState("5 5\n1 2 N\nLLMLMLMLMM");
+  const rovers: Rover[] = [];
+
+  const addInstructions = (e: React.MouseEvent<HTMLButtonElement>) => {
+    const plateau = createPlateau(instructions.split("\n")[0].split(" "));
+    for (let i = 1; i < instructions.split("\n").length; i = i + 2) {
+      const position = instructions.split("\n")[i].split(" ");
+      const moves = instructions.split("\n")[i + 1];
+      rovers.push(readInitialPosition(position, moves, plateau));
+      };
+      return console.log(rovers);
   }
-  
+
+  for (const rover of rovers) {
+    const roverrecordMoves(rover, plateau);
+    console.log(finalPosition)
+  }
+    
   return (
     <>
       <div>
-        <h1>Mars Rovers simulator App</h1>
-        <h2>For each rover you want to add, please click 'ADD NEW ROVER'. Provide the required information and click on 'SIMULATE'.</h2>
-        <button onClick={Rover}>ADD NEW ROVER</button>
-        <button onClick={handleSimulation}>SIMULATE</button>
+        <h1>Mars Rovers Simulator App</h1>
+        <h2>Insert instructions and click 'SIMULATE'.</h2>
+        <textarea
+          value={instructions}
+          placeholder='Add here'
+          onChange={(e) => setInstructions(e.target.value)}
+          rows={10}
+          cols={30}
+        />
       </div>
-
+      <div>
+         <button onClick={(e) => addInstructions(e)}>SIMULATE</button>
+      </div>
     </>
   )
 }
 
 export default App;
+
+// const input1 = [
+//     "5 5",
+//     "1 2 N",
+//     "LMLMLMLMM",
+//     "3 3 E",
+//     "MMRMMRMRRM",
+//     "2 5 W",
+//     "MLMLLRMMM"
+// ]
+
--- src/Rover.tsx
+++ src/Rover.tsx
@@ -1,55 +1,95 @@
-import { useState } from 'react';
+export const createPlateau = (input: string[]) => {
+    const maxX = parseInt(input[0]);
+    const maxY = parseInt(input[1]);
+    return { maxX, maxY };
+}
 
-export const Rover = () => {
-    const [newCoordinates, setNewCoordinates] = useState<string>('');
-    const [newMovements, setNewMovements] = useState<string>('');
+export type Plateau = ReturnType<typeof createPlateau>;
+export type Rover = ReturnType<typeof readInitialPosition>;
 
-    const handleInputChange1 = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setNewCoordinates(e.target.value);
-    };
-    const handleInputChange2 = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setNewMovements(e.target.value);
-    };
+export const readInitialPosition = (position: string[], moves: string, plateau: Plateau) => {
+    const x = parseInt(position[0]);
+    const y = parseInt(position[1]);
+
+    if (x > plateau.maxX || y > plateau.maxY || x < 0 || y < 0) {
+        throw new Error("Out of bounds.");
+    }
+    const dir = position[2];
+
+    if (!cardinals.includes(dir)) {
+        throw new Error("Invalid direction.");
+    }
+
+    return { x, y, dir, moves }
+}
+
+export const cardinals = ['N', 'E', 'S', 'W'];
 
 
-    const handleCoordinates = () => {
-        if (newCoordinates.trim() !== '') {
-            onClick(newCoordinates.trim());
-            setNewCoordinates('');
+export const rotateLeft = (input: string) => {
+    const index = cardinals.indexOf(input);
+
+    if (index === 0) {
+        return cardinals[cardinals.length - 1]
+    }
+
+    return cardinals[index - 1];
+}
+
+
+export const rotateRight = (input: string) => {
+    const index = cardinals.indexOf(input);
+
+    if (index === cardinals.length - 1) {
+        return cardinals[0]
+    }
+
+    return cardinals[index + 1];
+}
+
+export const moveForward = (x: number, y: number, dir: string) => {
+    if (dir === 'W') {
+        return { newX: x - 1, newY: y }
+    }
+
+    if (dir === 'E') {
+        return { newX: x + 1, newY: y }
+    }
+
+    if (dir === 'N') {
+        return { newX: x, newY: y + 1 }
+    }
+
+    if (dir === 'S') {
+        return { newX: x, newY: y - 1 }
+    }
+
+    return { newX: x, newY: y }
+
+}
+
+export const recordMoves = (rover: Rover, plateau: Plateau) => {
+    let { dir, x, y } = rover;
+
+    for (const move of rover.moves) {
+        if (move === 'R') {
+            dir = rotateRight(dir);
         };
-    };
 
-    const handleMovements = () => {
-        if (newMovements.trim() !== '') {
-            onClick(newMovements.trim());
-            setNewMovements('');
+        if (move === 'L') {
+            dir = rotateLeft(dir);
         };
-    };
-    
-    return (
-        <>
-            <div>
-                <input
-                    type="text"
-                    value={newCoordinates}
-                    onChange={handleInputChange1}
-                    placeholder='Add the coordinates of the rover. ex: 1 2 N'
-                />
-                <button onClick={handleCoordinates}>ADD</button>
-            </div>
 
-            <div>
-                <input
-                    type="text"
-                    value={newMovements}
-                    onChange={handleInputChange2}
-                    placeholder='Add the list of movements of the rover. ex: LMLMLMLMM'
-                />
-                <button onClick={handleMovements}>ADD</button>
-            </div>
+        if (move === 'M') {
+            const { newX, newY } = moveForward(x, y, dir);
+            
+            if (newX > plateau.maxX || newY > plateau.maxY || newX < 0 || newY < 0) {
+                return { x, y, dir };
+            }
 
-        </>
-
-    );
-};
-
+            x = newX;
+            y = newY;
+        };
+    } 
+    return `${x} ${y} ${dir}`;
+}
